9f6cab5381986ed76e4f883dbcaf2d23af66c02a
Byron <byronh@gmail.com>
2014-05-20 13:44:31 -0700

    Improved entry point and added astro::Exception

---
+++ src/Exception.h
@@ -0,0 +1,13 @@
+#include <stdexcept>
+#include <string>
+
+namespace astro
+{
+	class Exception : public std::runtime_error
+	{
+
+	public:
+		explicit Exception(const std::string & msg) : std::runtime_error(msg) {}
+
+	};
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,76 @@
 #include <iostream>
-
 #include "gl_core_3_3.h"
 #include <GLFW/glfw3.h>
-#include <glm/vec3.hpp>
-#include <glm/ext.hpp>
 
-#include <cstdio>
+#include "Exception.h"
 
-int main(void)
+
+void run()
 {
-
-	std::cout << "Hello World!" << std::endl;
-	glm::vec3 vec(0.5f, 0.5f, 0.5f);
-	std::cout << "Testing glm: " << glm::to_string(vec) << std::endl;
-
+	
+	// Initialize GLFW
 	GLFWwindow* window;
-
 	if (!glfwInit())
-		return -1;
+	{
+		throw astro::Exception("Failed to initialize GLFW!");
+	}
 
+	// Use OpenGL version 3.3 - forward compatibility needed for Mac OS X
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 7);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// Create GLFW window as OpenGL context
 	window = glfwCreateWindow(256, 128, "Hello World", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
-		std::cout << "Failed to create GLFW window!" << std::endl;
-		std::cin.get();
-		return -1;
+		throw astro::Exception("Failed to create window! Your hardware must support OpenGL 3.3 or higher.");
 	}
-
 	glfwMakeContextCurrent(window);
+
+	// Enable vertical sync
 	glfwSwapInterval(1);
 
-	if (ogl_LoadFunctions() == ogl_LOAD_SUCCEEDED)
+	// Load OpenGL extensions via glLoadGen
+	if (ogl_LoadFunctions() != ogl_LOAD_SUCCEEDED)
 	{
-		printf("OpenGL version supported by this platform (%s): \n", glGetString(GL_VERSION));
-
-		while (!glfwWindowShouldClose(window))
-		{
-			glClearColor(0.0f, 0.5f, 0.25f, 0.0f);
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-			glfwSwapBuffers(window);
-			glfwPollEvents();
-		}
-
 		glfwDestroyWindow(window);
 		glfwTerminate();
-		return 0;
-
-	} else {
-		std::cout << "Failed to load ogl functions!" << std::endl;
-		std::cin.get();
-
-		glfwDestroyWindow(window);
-		glfwTerminate();
-		return -1;
+		throw astro::Exception("Failed to load extensions for OpenGL 3.3!");
 	}
+	std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+
+	// Main loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.0f, 0.5f, 0.25f, 0.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Shut down
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
+
+int main(void)
+{
+	try
+	{
+		run();
+	}
+	catch (astro::Exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+		std::cerr << "Press any key to continue... ";
+		std::cin.get();
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
 
 }
