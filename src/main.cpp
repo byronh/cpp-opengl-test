679a84d12aeb0b9d883a783b33beed448fa2b0ec
Byron <byronh@gmail.com>
2014-05-20 13:47:39 -0700

    Fixed OpenGL version window hint

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ void run()
 	// Use OpenGL version 3.3 - forward compatibility needed for Mac OS X
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 7);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
