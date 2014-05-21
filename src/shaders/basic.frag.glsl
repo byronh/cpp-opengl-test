6aa2abbc16fe74d19b570777a443449f483a5f7f
Byron <byronh@gmail.com>
2014-05-20 23:31:39 -0700

    Added texture

--- src/shaders/basic.frag.glsl
+++ src/shaders/basic.frag.glsl
@@ -1,8 +1,5 @@
 #version 150
 
-out vec4 finalColor;
-
 void main() {
-    // set every drawn pixel to white
-    finalColor = vec4(1.0, 1.0, 1.0, 1.0);
+    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
--- src/shaders/basic.vert.glsl
+++ src/shaders/basic.vert.glsl
@@ -1,8 +1,7 @@
 #version 150
 
-in vec3 vert;
+attribute vec3 vert;
 
 void main() {
-    // does not alter the vertices at all
     gl_Position = vec4(vert, 1);
 }
